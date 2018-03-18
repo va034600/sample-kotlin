81ee29d14ac927c7afbc19f5e7845845291f5f9b
va034600 <va034600@gmail.com>
2018-03-18 15:26:51 +0900

    sample spring boot jpa

--- sample-spring-boot-jpa/src/main/kotlin/com/example/controller/HelloWorldController.kt
+++ sample-spring-boot-jpa/src/main/kotlin/com/example/controller/HelloWorldController.kt
@@ -2,9 +2,12 @@ package com.example.controller
 
 import com.example.service.SampleService
 import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.ModelAttribute
 
 @RestController
 class HelloWorldController(
